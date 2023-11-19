7a5d4c323d08fecd7a54ae67051b4f5a31c29678
hundochkatt <144358437+hundochkatt@users.noreply.github.com>
2023-11-19 14:54:48 +0100

    Create heder.js

---
+++ public/heder.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Heder = () => {
+    return (
+    <header>
+    <img class="backround-lines" src="imagens/background-lines.svg" alt=""/>
+  
+     <Section class="nav-Section">
+       <div>=class"container" >
+        <a href="home.html"><img class="logotaype" src="images/logyape.svg" alt= ""></a>
+         <nav>
+                  </div> class="firtst-row">
+                     <div>class="copany-info">
+              <span><i class="fa-regular fa-phone-volume"></i>+46(8) 121 470 50</span>
+              <span><i class="fa-regular fa-envelope></i>info@crito.com</span>
+              <span><i class="fa-sharp fa-regular fa-location-dot"></i> sveavägen 31,111 34 stockholm</span>
+       </div>
+          </div class="Social-Media">
+             <a href="Http//facebook.Com"><i class="fa-brands fa-facbock"></i></a>
+             <a href="Http//twitter.Com"><i class="fa-brands fa-twitter" ></i></a>
+             <a href="Http//instagram.Com"><i class="fa-brandsfa-instagram  ></i></a>
+             <a href="Http//linkedin.Com"><i class="fa-brands fa-linktin  ></i></a>
+            </div>
+       </div 
+       <div class="second-row"> 
+      <div "class=navbar">
+      <a class="active"href=home.html">home</a>
+      <a class "service.html">Service</a>
+      <a href="news.html">news</a>
+      <a href=contanct,html"
+      <a href=."conetatct,html">Contract<a>
+      <a class "btn-theme" href="login.html">Login <i class"fa-solind fa-arrow-up rigt"></a>
+      </div>
+      </nav/>
+      </div>
+</section>
+
+<!--sowecase selection-->
+     <setion class="=showcse-section">
+    <div class="contener">
+    <div class="content-column">
+    <h1>we provosede the best consulting serices <h1>
+<p>Esetablih your vision and and value proostion and and turn them into prptotypes.</p>
+</div>class"button links">
+<a class=btn-"theme"herf= "consulting.html">Get consulting<i class fa-solid fa arrow-up
+<a class=btn-"theme"href=
+
+/div></a>
+  /<div></a>  
+</div> class="image-clommn">
+<img scr images/sowcre imge svg alt""/>  
+</div>
+</div>
+   </Section>
+</header> 
+  )
+}
+
+export default Heder
